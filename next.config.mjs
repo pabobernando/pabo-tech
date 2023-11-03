daa14612a6368ca9c8e489156b6fe749ebf935b4
pabo bernando <pabobernando@desolator.local>
2023-11-03 17:31:14 +0700

    feat: update config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  serverRuntimeConfig: {
+    API_URL: process.env.API_URL,
+  },
+  // Will be available on both server and client
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
 }
 
 const withMDX = nextMDX({
